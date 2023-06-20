16555b0eeb5f9ce45ca8e2de4a2220d1d3757675
João Gabriel <118997962+joaog0liveira@users.noreply.github.com>
2023-06-20 08:05:51 -0300

    20/06/2023

---
+++ src/br/com/faculdade/projetopoo/ProjetoPOO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.faculdade.projetopoo;
+
+/**
+ *
+ * @author joaog
+ */
+public class ProjetoPOO {
+    public static void main(String [] args)throws Exception {
+        Initialize init = new Initialize();
+        init.generateDB();
+        
+    }
+}
